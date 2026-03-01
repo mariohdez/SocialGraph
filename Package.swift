3b8a4e45049ef56a41079403690430a746127245
Mario Hernandez <mhmario963@gmail.com>
2026-03-01 17:01:50 -0500

    scaffold social graph server backed by hummingbird.

--- Package.swift
+++ Package.swift
@@ -1,22 +1,28 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "SocialGraph",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SocialGraph",
-            targets: ["SocialGraph"]
-        ),
+        .library(name: "SocialGraph", targets: ["SocialGraph"]),
+        .executable(name: "SocialGraphServer", targets: ["SocialGraphServer"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/hummingbird-project/hummingbird.git",
+            from: "2.0.0"
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SocialGraph"
+        ),
+        .executableTarget(
+            name: "SocialGraphServer",
+            dependencies: [
+                "SocialGraph",
+                .product(name: "Hummingbird", package: "hummingbird"),
+            ]
         ),
         .testTarget(
             name: "SocialGraphTests",
---
+++ Sources/SocialGraphServer/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  SocialGraph
+//
+//  Created by Mario Hernandez on 3/1/26.
+//
+
+import Hummingbird
+import SocialGraph
