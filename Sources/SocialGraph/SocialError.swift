6a57b050d6d6d5c7eb292663f8ec5d2f65d2aad2
Mario Hernandez <mhmario963@gmail.com>
2026-03-01 15:14:07 -0500

    define user within the social graph.

---
+++ Sources/SocialGraph/SocialError.swift
@@ -0,0 +1,17 @@
+//
+//  SocialError.swift
+//  SocialGraph
+//
+//  Created by Mario Hernandez on 3/1/26.
+//
+
+/// An enumeration of errors that can be thrown by the social graph
+/// operations.
+enum SocialError: Error {
+    /// The friends list of a user cannot contain themselves.
+    case befriendingSelf
+    
+    /// The friends list of a user cannnot contain the given
+    /// username more than once.
+    case alreadyFriend(username: String)
+}
